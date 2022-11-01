cc8c649a0ac2c0b1c68e7f6221de2aff4d3eddaf
Cristopher Ponce <46466934+devgrowth4u@users.noreply.github.com>
2022-11-01 17:04:30 -0300

    Numeor de fibonacci 2

--- script.js
+++ script.js
@@ -1,53 +1,3 @@
-/*
-    0, 1, 1, 2, 3, 5, 8, 13...
-    fn = f(n - 1) + f(n - 2)
-    cuando n >= 2
-
-
-addEventListener('load', function() {
-    ejecutarPrograma();
-    function ejecutarPrograma() {
-        let n = parseInt(prompt('Cantidad de número:'));
-        if (Number.isInteger(n) && n > 0) {
-            let arrayFibonacci = generarFibonacci(n);
-            imprimirArray(arrayFibonacci);
-            /*generarFibonacci(n);
-        } else {
-            console.log('El número ingresado no es un entero > 0');
-        }
-    }
-
-    function generarFibonacci(n) {
-        /*let f0 = 0;
-        let f1 = 1;
-        let fn = null;
-        let fn = [0, 1];
-        if (n <= 1) {
-            /*console.log(f0);
-            console.log(f1);
-            return;
-            return fn;
-        }
-        /*console.log(f0);
-        console.log(f1);
-        for (let i = 2; i <= n; i++) {
-            fn[i] = fn[i - 1] + fn[i - 2];
-            /*fn = f1 + f0;
-            f0 = f1;
-            f1 = fn;
-            console.log(fn);
-            
-        }
-        return fn;
-    }
-
-    function imprimirArray(array) {
-        for(let i = 0; i < array.length; i++) {
-            console.log(array[i]);
-        }
-    }
-});
-*/
 
 // Desarrollo
 // primera prueba de Fibonacci con 0, 1, y solo llegar hasta el 1000
@@ -63,11 +13,12 @@ function SerieFibonacci(limit){
     return Fibonacci;
 }
 
-const serieFibonacci= SerieFibonacci(1000);
+const serieFibonacci= SerieFibonacci(1001);
 console.log(serieFibonacci);
 
 
-function Par(){
+function Pares(){
+
 
 }
 
