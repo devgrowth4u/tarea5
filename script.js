d04d3faba5178013c95e10937c56d42b5da7431a
Cristopher Ponce <46466934+devgrowth4u@users.noreply.github.com>
2022-11-01 18:41:48 -0300

    Parees e impares

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
 // Desarrollo
-// primera prueba de Fibonacci con 0, 1, y solo llegar hasta el 1000
-
+//Mostrar en consola la secuencia de Fibonacci:
+    //a. Entre los números 0 y 1000.
+    //b. Números pares entre 0 y 1000.
+    //c. Números impares entre 0 y 1000.
 
 function SerieFibonacci(limit){
 
@@ -13,15 +15,41 @@ function SerieFibonacci(limit){
     return Fibonacci;
 }
 
-const serieFibonacci= SerieFibonacci(1001);
+const serieFibonacci= SerieFibonacci(10);
 console.log(serieFibonacci);
 
+function ParesFibonnaci(limit){
 
-function Pares(){
+const Fibonacci = [0,1];
+    for(let i = 2; i < limit; i++){
+        Fibonacci [i] = Fibonacci[i-1] + Fibonacci[i-2];
+        if (Fibonacci[i]%2===0){
 
+            console.log("El número par es: " + Fibonacci[i]);
+        }
+        
+    }
+    return Fibonacci;
 
 }
 
-function impar(){
+const serieFibonaccipar= ParesFibonnaci(10);
+console.log(serieFibonaccipar);
 
-}
+function imparesFibonacci(limit){
+    
+        const Fibonacci = [0,1];
+            for(let i = 2; i < limit; i++){
+                Fibonacci [i] = Fibonacci[i-1] + Fibonacci[i-2];
+                if (Fibonacci[i]%2!==0){
+        
+                    console.log("El número impar es: " + Fibonacci[i]);
+                }
+        
+            }
+            return Fibonacci;
+        
+        }
+const serieFibonacciimpar= imparesFibonacci(10);
+console.log(serieFibonacciimpar);
+        
